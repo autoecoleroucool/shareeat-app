a2f09acc27f76ed8c632eabae6803645c5317de5
gurkan kestane <roucoolecole@gmail.com>
2026-03-03 00:10:24 +0100

    Updated netlify.toml

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,19 @@
-const CACHE_NAME = 'shareeat-v1';
+const CACHE_NAME = 'shareeat-v2';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    }).then(() => clients.claim())
+  );
 });
 
 self.addEventListener('push', (event) => {
